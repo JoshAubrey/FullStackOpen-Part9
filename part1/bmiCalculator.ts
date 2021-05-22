fac9ebfd48caa5855083657a56f31b0afde21906
JoshAubrey <joshaubrey7@gmail.com>
2021-05-22 11:24:50 -0500

    create bmi and exercise calc ts programs

---
+++ part1/bmiCalculator.ts
@@ -0,0 +1,43 @@
+interface heightWeight {
+    height: number;
+    weight: number;
+}
+
+const parseBmiArguments = (args: Array<string>): heightWeight => {
+    if (typeof args[2] === 'undefined' || typeof args[3] === 'undefined') throw new Error('Missing arguments! Format: npm run calculateBmi <heightInCm> <weightInKg>')
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        height: Number(args[2]),
+        weight: Number(args[3])
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+}
+
+const calculateBmi = (height: number, weight: number ) : String => {
+    //height (in centimeters) and weight (in kilograms)
+    //dividing the weight in kilograms by the height in centimeters squared, 
+    //and then multiplying the result by 10,000
+
+    //underweight (under 18.5 kg/m2), normal weight (18.5 to 25), 
+    //overweight (25 to 30), and obese (over 30)
+
+    const bmi = (weight / (height * height)) * 10000
+
+    if (bmi < 18.5) return 'Underweight'
+    else if (bmi <= 25) return 'Normal (healthy weight)'
+    else if (bmi > 25 && bmi <=30 ) return 'Overweight'
+    else if (bmi > 30) return 'Obese'
+
+}
+
+//console.log(calculateBmi(180, 74))
+try {
+    const { height, weight } = parseBmiArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+} catch (e) {
+    console.log('Error, something bad happened, message: ', e.message);
+}
+//npm run calculateBmi 180 74
